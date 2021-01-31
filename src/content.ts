9b4aa6931f0257e1f4b63ee166172214abd4e046
dabreadman <tany3@tcd.ie>
2021-01-31 22:18:16 +0000

    Working prototype for preset data from local

--- src/content.ts
+++ src/content.ts
@@ -1,10 +1,8 @@
-const sectionTitle = document.getElementsByClassName('section-title');
-if (sectionTitle) {
-  sectionTitle[0].innerHTML = 'hello worlds';
-}
+import GetResponseTypeFromSkipTimes from './types/pages/skip_time_types';
+import { skipOpEd } from './utils/on_page';
 
 // Helper function to capitalize the first letter
-function capitalizeFirstLetter(str: String) {
+function capitalizeFirstLetter(str: string) {
   return str[0].toUpperCase() + str.slice(1);
 }
 
@@ -12,22 +10,25 @@ function capitalizeFirstLetter(str: String) {
  * @returns A tuple of (providerName, animeId and episodeNumber)
  */
 async function getDataFromCurrentUrl() {
-  const { pathname, hostname } = await window.location;
+  const { pathname, hostname } = window.location;
   let regex = /(?:[^.\n]*\.)?([^.\n]*)(\..*)/;
-  const providerName = capitalizeFirstLetter(hostname.replace(regex, ''));
+  const providerName = capitalizeFirstLetter(hostname.replace(regex, '$1'));
 
-  let idEpsNumber: String[] = [];
+  let idEpsNumber: string[] = [];
   let cleansedPath;
+
   switch (providerName) {
     case 'Aniwatch':
-      regex = /anime\//;
+      regex = /\/anime\//;
       cleansedPath = pathname.replace(regex, '');
       regex = /\//;
       idEpsNumber = cleansedPath.split(regex);
       break;
     case 'Gogoanime':
+      regex = /\//;
+      cleansedPath = pathname.replace(regex, '');
       regex = /-episode-/;
-      idEpsNumber = pathname.split(regex);
+      idEpsNumber = cleansedPath.split(regex);
       break;
     case '9Anime':
       regex = /.*\./;
@@ -39,86 +40,71 @@ async function getDataFromCurrentUrl() {
       idEpsNumber = ['', ''];
       break;
   }
-  const result = idEpsNumber.unshift(providerName);
+  const [animeId, episodeNumber] = idEpsNumber;
+  const result = {
+    providerName,
+    animeId,
+    episodeNumber,
+  };
   return result;
 }
 
-/** Get MAL id from API with provided providerName and anime_id
+/** Get MAL id from API with provided providerName and animeId
  * @param providerName Provider name, first letter must be capitalized if possible
- * @param anime_id Anime id for the specified provider, no uniform format
+ * @param animeId Anime id for the specified provider, no uniform format
  * @returns malId
  */
-async function getMALId(providerName: String, anime_id: String) {
+async function getMALId(providerName: string, animeId: string) {
+  const res = await fetch(
+    `https://api.malsync.moe/page/${providerName}/${animeId}`
+  );
+  if (!res.ok) throw new Error('MAL ID not found');
+  const json = await res.json();
+  return json.malId;
+}
+
+async function getDataFromAPI(
+  malId: string,
+  episodeNumber: string,
+  skipType: 'op' | 'ed'
+): Promise<GetResponseTypeFromSkipTimes> {
+  const res = await fetch(
+    `http://localhost:5000/api/v1/skip-times/${malId}/${episodeNumber}?type=${skipType}`
+  );
+  const json = await res.json();
+  return json;
+}
+
+async function main() {
+  const webpageData = await getDataFromCurrentUrl();
   try {
-    const res = await fetch(
-      `https://api.malsync.moe/page/${providerName}/${anime_id}`
-    );
-    const json = await res.json();
-    return json.malId;
+    const malId = await getMALId(webpageData.providerName, webpageData.animeId);
+    const { episodeNumber } = webpageData;
+    const [openingSkip, endingSkip] = await Promise.all([
+      getDataFromAPI(malId, episodeNumber, 'op'),
+      getDataFromAPI(malId, episodeNumber, 'ed'),
+    ]);
+    const player: HTMLVideoElement | null = document.querySelector('#player');
+    if (player) {
+      let openingSkipper;
+      let endingSkipper;
+      player.addEventListener('loadedmetadata', () => {
+        [openingSkipper, endingSkipper] = skipOpEd(
+          openingSkip,
+          endingSkip,
+          player
+        );
+      });
+      console.log({ openingSkipper, endingSkipper });
+    }
   } catch (err) {
-    throw new Error('No MalID');
+    console.error(err);
   }
 }
 
-/** Get most voted time segments from given skip times
- * @param entries entries[0] contains OP skip times; entries[1] contains ED skip times
- * @returns [OP,ED]
- */
-function timeSkip(entries: any[]) {
-  const opjson = entries[0].map((entry) => {
-    let {
-      skip_id,
-      anime_id,
-      episode_number,
-      provider_name,
-      skip_type,
-      votes,
-      start_time,
-      end_time,
-      episode_length,
-      submit_date,
-    } = entry;
-    return entry;
-  });
+main();
 
-  const edjson = entries[0].map((entry) => {
-    let {
-      skip_id,
-      anime_id,
-      episode_number,
-      provider_name,
-      skip_type,
-      votes,
-      start_time,
-      end_time,
-      episode_length,
-      submit_date,
-    } = entry;
-    return entry;
-  });
-
-  let maxVote = Number.MIN_SAFE_INTEGER;
-  let maxOpIndex = -1;
-  // Get index of most voted OP skip time
-  for (let i = 0; i < opjson.length; i += 1) {
-    if (parseInt(opjson[i][6], 10) > maxVote) {
-      maxOpIndex = i;
-    }
-  }
-
-  let maxEdIndex = -1;
-  maxVote = Number.MIN_SAFE_INTEGER;
-  // Get index of most voted ED skip time
-  for (let i = 0; i < opjson.length; i += 1) {
-    if (parseInt(edjson[i][6], 10) > maxVote) {
-      maxEdIndex = i;
-    }
-  }
-
-  return [opjson[maxOpIndex], edjson[maxEdIndex]];
-}
 exports = {
-  timeSkip,
   getMALId,
   capitalizeFirstLetter,
   getDataFromCurrentUrl,
