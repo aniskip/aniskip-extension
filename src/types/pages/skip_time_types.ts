906737678d646cb52d41d4231b679c7780c90f12
dabreadman <tany3@tcd.ie>
2021-01-31 13:19:21 +0000

    Add SkipTimesType (temp type)

---
+++ src/types/pages/skip_time_types.ts
@@ -0,0 +1,14 @@
+interface SkipTimesType {
+  skip_id: string;
+  anime_id: number;
+  episode_number: number;
+  provider_name: string;
+  skip_type: string;
+  votes: number;
+  start_time: number;
+  end_time: number;
+  episode_length: number;
+  submit_date: Date;
+}
+
+export default SkipTimesType;
