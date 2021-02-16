c45d2497f6cd1f0de90a1464aebb9c5c41496d54
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-02-16 00:19:28 +0000

    Fix 9anime provider name

--- src/pages/nineanime/page.ts
+++ src/pages/nineanime/page.ts
@@ -1,6 +1,14 @@
 import BasePage from '../base_page';
 
 class Nineanime extends BasePage {
+  getProviderName(): string {
+    const domainName = this.hostname.replace(
+      /(?:[^.\n]*\.)?([^.\n]*)(\..*)/,
+      '$1'
+    );
+    return domainName;
+  }
+
   getIdentifier(): string {
     const cleansedPath = this.pathname.replace(/.*\./, '');
     return cleansedPath.split(/\/ep-/)[0];
