ea27b88f6677fb34ed4b56c7f2a02fcdd1ad51c7
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-05-09 20:28:37 +0100

    Change min height

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import SettingsPage from './SettingsPage';
 
 const App: React.FC = () => (
-  <div className="min-w-[400px] min-h-[400px]">
+  <div className="min-w-[400px] min-h-[450px]">
     <Navbar />
     <div className="font-sans mx-auto max-w-screen-md sm:px-4 py-10">
       <SettingsPage />
