d59ecca7d520fcc35fcac3b33c3925faeea6f0c7
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-05-09 13:19:02 +0100

    Change settings style

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -5,7 +5,7 @@ import SettingsPage from './SettingsPage';
 const App: React.FC = () => (
   <div className="min-w-[400px] min-h-[400px]">
     <Navbar />
-    <div className="font-sans mx-auto max-w-screen-lg px-4 py-10">
+    <div className="font-sans mx-auto max-w-screen-md sm:px-4 py-10">
       <SettingsPage />
     </div>
   </div>
