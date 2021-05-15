373fe6aa74e74149d0903c8b0e1180da823d01f3
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-05-15 23:39:46 +0100

    Change padding

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -5,7 +5,7 @@ import SettingsPage from './SettingsPage';
 const App: React.FC = () => (
   <div className="min-w-[350px] min-h-[400px]">
     <Navbar />
-    <div className="font-sans mx-auto max-w-screen-lg sm:px-4 sm:py-10">
+    <div className="font-sans mx-auto max-w-screen-lg sm:px-8 sm:py-10">
       <SettingsPage />
     </div>
   </div>
