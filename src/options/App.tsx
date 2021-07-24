b9bab1d6b113d48973dd874c97a3719ca0b42b91
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-07-24 14:32:13 +0100

    Change to JSX element

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Navbar } from './components/Navbar';
 import { SettingsPage } from './SettingsPage';
 
-export const App: React.FC = () => (
+export const App = (): JSX.Element => (
   <div className="min-w-[350px] min-h-[400px]">
     <Navbar />
     <div className="font-sans mx-auto max-w-screen-lg sm:px-8 sm:py-10">
