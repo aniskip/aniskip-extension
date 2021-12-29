00d624b781d6e72f35b6b025431481c0819b2499
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-12-29 00:27:42 +0000

    Add bundle analysis

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { default: merge } = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const WebExtPlugin = require('@mantiquillal/web-ext-plugin');
 const webExtConfig = require('../web-ext.config');
 const commonConfig = require('./webpack.common.config');
@@ -10,6 +12,7 @@ module.exports = merge(commonConfig, {
     minimizer: [new CssMinimizerPlugin(), '...'],
   },
   plugins: [
+    ...(process.env.ANALYZE === 'true' ? [new BundleAnalyzerPlugin()] : []),
     new WebExtPlugin({
       sourceDir: webExtConfig.sourceDir,
       ...(!process.env.BUILD || process.env.BUILD === 'false'
