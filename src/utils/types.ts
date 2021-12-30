03c04d152a6bf695da9146524fd0911e1badd436
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-12-30 19:34:24 +0000

    Fix keyboard typing

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,12 @@
 export type StateSlice<TSlice, TSliceKey extends keyof any> = {
   [Key in TSliceKey]: TSlice;
 };
+
+export type SliceActions<T> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => infer A ? A : never;
+}[keyof T];
+
+export type ComponentPropsWithAs<TTag extends React.ElementType> = {
+  as?: TTag;
+  children?: React.ReactNode;
+} & Omit<React.ComponentProps<TTag>, 'children'>;
