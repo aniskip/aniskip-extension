b9cb64da8eb7c279e6c697fe422ce6755cf7f46b
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2022-01-05 20:35:31 +0000

    Fix keybind style issue

--- src/options/components/SettingsPage/SettingsPage.tsx
+++ src/options/components/SettingsPage/SettingsPage.tsx
@@ -252,7 +252,7 @@ export function SettingsPage(): JSX.Element {
   const renderKeybind = (keybind: string, type?: KeybindType): JSX.Element => {
     if (!keybind) {
       return (
-        <div className="px-4 py-2 border-transparent rounded text-sm bg-primary border border-gray-300 text-white font-medium focus:outline-none sm:w-auto w-full">
+        <div className="px-4 py-2 border-transparent rounded text-sm bg-primary border border-gray-300 text-white font-medium focus:outline-none">
           Add keybind
         </div>
       );
