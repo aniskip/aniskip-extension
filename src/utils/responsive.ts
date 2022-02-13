ceb87590fa3983979c1053eda037cde9560634a1
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2022-02-13 19:50:21 +0000

    Fix skip button fullscreen style bug #174

--- src/utils/responsive.ts
+++ src/utils/responsive.ts
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react';
 
 /**
+ * Checks if the screen is in fullscreen mode.
+ */
+const checkIsFullscreen = (): boolean =>
+  !!document.fullscreenElement ||
+  // Hack to check fullscreen when next episode plays and the video does not
+  // exit fullscreen mode.
+  window.screen.height === window.innerHeight;
+
+/**
  * Returns if the browser is in full screen.
  */
 export const useCheckIsFullscreen = (): { isFullscreen: boolean } => {
-  const [isFullscreen, setIsFullscreen] = useState(false);
+  const [isFullscreen, setIsFullscreen] = useState(checkIsFullscreen());
 
   useEffect(() => {
     const fullscreenHandler = (): void => {
-      setIsFullscreen(!!document.fullscreenElement);
+      setIsFullscreen(checkIsFullscreen());
     };
 
     document.addEventListener('fullscreenchange', fullscreenHandler);
