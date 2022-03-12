4d2c9ffa18b7dbdb5864d811d29d7a870a0c0b80
idonotknowwhoiam <marcusymka@gmail.com>
2022-03-13 00:01:57 +0300

    Add new player for animego page

---
+++ src/players/kodik/kodik.ts
@@ -0,0 +1,41 @@
+import { getDomainName } from '../../utils';
+import { BasePlayer } from '../base-player';
+import { Metadata } from '../base-player.types';
+import metadata from './metadata.json';
+
+export class Kodik extends BasePlayer {
+  constructor() {
+    super(metadata);
+  }
+
+  static getMetadata(): Metadata {
+    return metadata;
+  }
+
+  getVideoContainer(): HTMLElement | null {
+    return super.getContainerHelper(
+      this.metadata.selectorStrings.default!.videoContainerSelectorString
+    );
+  }
+
+  getVideoControlsContainer(): HTMLElement | null {
+    const domainName = getDomainName(window.location.hostname);
+    const { selectorStrings } = this.metadata;
+
+    return super.getContainerHelper(
+      selectorStrings[domainName]?.videoControlsContainerSelectorString ??
+        selectorStrings.default!.videoControlsContainerSelectorString
+    );
+  }
+
+  getSettingsButtonElement(): HTMLElement | null {
+    const domainName = getDomainName(window.location.hostname);
+    const { selectorStrings } = this.metadata;
+
+    return super.getContainerHelper(
+      selectorStrings[domainName]
+        ?.injectMenusButtonsReferenceNodeSelectorString ??
+        selectorStrings.default!.injectMenusButtonsReferenceNodeSelectorString
+    );
+  }
+}
